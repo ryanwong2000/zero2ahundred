3af9fc4338f12247288a35bdb195b4d5ed2b85d0
ryanwong2000 <56795999+ryanwong2000@users.noreply.github.com>
2022-01-27 09:56:22 -0500

    seperated app and button components

--- src/App.js
+++ src/App.js
@@ -1,43 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Buttons from './components/Button';
 
 function App() {
-  const numberOfButtons = 101;
-
-  // https://bost.ocks.org/mike/shuffle/
-  //shuffles an array of numbers of size n
-  function shuffleNumbers(n) {
-    //populate numbers array
-    const array = [];
-    for (let i = 0; i < n; i++) {
-      array.push(i);
-    }
-
-    let currentIndex = array.length,
-      randomIndex;
-
-    // While there remain elements to shuffle...
-    while (currentIndex != 0) {
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-
-      // And swap it with the current element.
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex],
-        array[currentIndex]
-      ];
-    }
-
-    return array;
-  }
-
-  function handleClick(e) {}
-
-  const numbers = shuffleNumbers(numberOfButtons);
-  const buttons = numbers.map((number) => <button>{number}</button>);
-
-  return <div className="game">{buttons}</div>;
+  return (
+    <div className="game">
+      <Buttons />
+    </div>
+  );
 }
 
 export default App;
