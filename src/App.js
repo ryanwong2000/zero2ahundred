dc25a3556036850b0aaf8e79ec73a9cab6aa5339
ryanwong2000 <56795999+ryanwong2000@users.noreply.github.com>
2022-01-26 22:16:51 -0500

    creates randomized buttons

--- src/App.js
+++ src/App.js
@@ -2,24 +2,38 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const numberOfButtons = 100;
+
+  // https://bost.ocks.org/mike/shuffle/
+  function shuffleNumbers(numOfButtons) {
+    //populate numbers array
+    const array = [];
+    for (let i = 0; i < numOfButtons; i++) {
+      array.push(i);
+    }
+
+    let currentIndex = array.length,
+      randomIndex;
+
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex]
+      ];
+    }
+
+    return array;
+  }
+  const numbers = shuffleNumbers(numberOfButtons);
+  const buttons = numbers.map((number) => <button>{number}</button>);
+
+  return <div>{buttons}</div>;
 }
 
 export default App;
