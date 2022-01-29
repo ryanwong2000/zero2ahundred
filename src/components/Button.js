ad961d187a850e901aff16111898f9162ed39673
ryanwong2000 <56795999+ryanwong2000@users.noreply.github.com>
2022-01-28 23:02:39 -0500

    remove stuff

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,37 +5,5 @@ export default function Button(props) {
     margin: '3px'
   };
 
-  // const numberOfButtons = 101;
-
-  // // https://bost.ocks.org/mike/shuffle/
-  // //shuffles an array of numbers of size n
-  // function shuffleNumbers(n) {
-  //   //populate numbers array
-  //   const array = [];
-  //   for (let i = 0; i < n; i++) {
-  //     array.push(i);
-  //   }
-
-  //   let currentIndex = array.length,
-  //     randomIndex;
-
-  //   // While there remain elements to shuffle...
-  //   while (currentIndex != 0) {
-  //     // Pick a remaining element...
-  //     randomIndex = Math.floor(Math.random() * currentIndex);
-  //     currentIndex--;
-
-  //     // And swap it with the current element.
-  //     [array[currentIndex], array[randomIndex]] = [
-  //       array[randomIndex],
-  //       array[currentIndex]
-  //     ];
-  //   }
-
-  //   return array;
-  // }
-
-  // const numbers = shuffleNumbers(numberOfButtons);
-
   return <button style={style}>{props.number}</button>;
 }
