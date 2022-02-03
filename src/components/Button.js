42045b587bf33fe8b903fb8bfa4caf3fb012e94b
ryanwong2000 <56795999+ryanwong2000@users.noreply.github.com>
2022-02-02 22:30:48 -0500

    more styling

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 
 export default function Button(props) {
+  const z2red = '#a90000';
+  const z2pink = '#f0b2bc';
   const styles = {
     borderRadius: 4,
     border: 'none',
     padding: '8px 12px',
-    margin: 5
+    margin: 5,
+    color: 'white',
+    backgroundColor: z2red, //pink
+    opacity: 1,
+    mouseover: true
   };
+
+  if (props.number <= props.count) {
+    styles.opacity = 0.4;
+    styles.mouseover = false;
+  }
 
   return (
     <button style={styles} onClick={props.handleClick}>
