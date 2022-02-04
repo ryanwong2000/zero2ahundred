1fec1f3d03849fd0fdbc1f5a256eb38da180ef16
ryanwong2000 <56795999+ryanwong2000@users.noreply.github.com>
2022-02-03 23:24:45 -0500

    button css + animations

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,29 +1,18 @@
 import React, { Component } from 'react';
+import '../css/Button.css';
 
 export default function Button(props) {
   const z2red = '#a90000';
   const z2pink = '#f0b2bc';
-  const styles = {
-    borderRadius: 4,
-    border: 'none',
-    padding: '10px 12px',
-    margin: 5,
-    color: 'white',
-    backgroundColor: z2red, //pink
-    opacity: 1,
-    mouseover: true,
-    fontSize: 12
-  };
 
-  if (props.number < props.count) {
-    styles.opacity = 0.4;
-    styles.mouseover = false;
-  }
+  // if (props.number < props.count) {
+  //   styles.opacity = 0.4;
+  //   styles.mouseover = false;
+  // }
 
   return (
     <button
       type="button"
-      style={styles}
       onClick={props.handleClick}
       value={props.number}
       disabled={props.gameOver || props.number < props.count}
